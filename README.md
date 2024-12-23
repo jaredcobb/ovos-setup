11bf2897094294f89ae5aa0825bcfd3e2e97e700
Jared Cobb <github@jaredcobb.com>
2024-12-23 10:44:20 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ovos-setup
+# OVOS Setup
+
 Helper scripts to setup my own OVOS preferences
+
+## Base Setup
+
+Ensure that you have a fresh installation of the [raspOVOS](https://github.com/OpenVoiceOS/raspOVOS) image. Configure the defaults using the [Raspberry Pi Imager](https://www.raspberrypi.com/software/).
+
+## Initializer
+
+Run the following command to bootstrap additional packages and configurations. This will also customize the default installation.
+
+```
+sh -c "curl -s https://raw.githubusercontent.com/jaredcobb/ovos-setup/refs/heads/master/scripts/init.sh > init.sh && chmod +x init.sh && sudo ./init.sh && rm init.sh"
+```
---
+++ scripts/init.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+echo_info() {
+    echo -e "\033[94m[INFO]: $1\033[0m"
+}
+
+echo_warning() {
+    echo -e "\033[91m[WARNING]: $1\033[0m"
+}
+
+echo_bright() {
+    echo -e "\033[33m$1\033[0m"
+}
+
+prompt_bright() {
+    echo -e "\033[33m$1\033[0m"
+}
+
+prompt_bright "Would you like to initialize the hardware? (First time only) [Y/n]: "
+read init
+if [[ -z "$init" || $init == y* || $init == Y* ]]; then
+    echo_info "Initializing hardware..."
+    sudo raspi-config nonint do_change_locale en_US.UTF-8
+    sudo raspi-config nonint do_spi 0
+    sudo raspi-config nonint do_i2c 0
+fi
+
+prompt_bright "Would you like to update and upgrade apt? [Y/n]: "
+read update_apt
+if [[ -z "$init" || $init == y* || $init == Y* ]]; then
+    sudo apt -y update && sudo apt -y upgrade
+    sudo apt install -y vim
+fi
+
+echo_info "Refreshing logo..."
+if [ -f /home/ovos/.logo.sh ]; then
+    mv /home/ovos/.logo.sh /home/ovos/.logo.sh.bak.$(date +%Y%m%d_%H%M%S)
+fi
+if [ -f /home/ovos/.cli_login.sh ]; then
+    mv /home/ovos/.cli_login.sh /home/ovos/.cli_login.sh.bak.$(date +%Y%m%d_%H%M%S)
+fi
+wget -O /home/ovos/.logo.sh https://gist.githubusercontent.com/jaredcobb/f66facf9e9f4915a2dd4003f2f99e757/raw/7841b0e57b0fef1ff69998b8cdcf1f13e232c44a/logo.sh
+wget -O /home/ovos/.cli_login.sh https://gist.githubusercontent.com/jaredcobb/f66facf9e9f4915a2dd4003f2f99e757/raw/7841b0e57b0fef1ff69998b8cdcf1f13e232c44a/cli_login.sh
+
+echo "" >> /home/ovos/.bashrc
+echo "alias l='ls -la'" >> /home/ovos/.bashrc
+
+chown ovos:ovos /home/ovos/.logo.sh
+chown ovos:ovos /home/ovos/.cli_login.sh
+chown ovos:ovos /home/ovos/.bashrc
+
+echo "Done initializing the server..."
+echo
+
+source /home/ovos/.venvs/ovos/bin/activate
+
+prompt_bright "Would you like to purge the preinstalled skills and start fresh? [Y/n]: "
+read purge
+if [[ -z "$purge" || $purge == y* || $purge == Y* ]]; then
+    echo_info "Removing skills..."
+    uv pip list | grep skill | awk '{print $1}' | xargs uv pip uninstall
+fi
+
+prompt_bright "Would you like to update the core OVOS packages? [Y/n]: "
+read update
+if [[ -z "$update" || $update == y* || $update == Y* ]]; then
+    echo_info "Updating packages..."
+    uv pip install -c https://github.com/OpenVoiceOS/ovos-releases/raw/refs/heads/main/constraints-testing.txt -U --pre $(uv pip list --format=freeze | grep -E 'ovos-|skill-' | cut -d '=' -f 1)
+fi
+
+prompt_bright "Would you like to restart now? [Y/n]: "
+read restart
+if [[ -z "$restart" || $restart == y* || $restart == Y* ]]; then
+    echo_info "See you soon!"
+    sudo reboot now
+fi
